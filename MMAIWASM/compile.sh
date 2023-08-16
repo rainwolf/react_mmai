d5902cb3bd6eee0562bf2ea86baacfaf03e96d4a
rainwolf <rainwolf@submanifold.be>
2023-08-16 16:48:09 +0200

    fix compile script

--- MMAIWASM/compile.sh
+++ MMAIWASM/compile.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-emcc Ai.cpp mmai.cpp -O3 -o ai.wasm -s WASM=1 -s "EXTRA_EXPORTED_RUNTIME_METHODS=['ccall']" -std=c++11 --embed-file files/
+emcc Ai.cpp mmai.cpp -O3 -o ai.js -s WASM=1 -s "EXPORTED_RUNTIME_METHODS=['ccall']" -std=c++11 --embed-file files/
