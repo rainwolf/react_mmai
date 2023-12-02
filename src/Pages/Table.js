230fc3368f73e2244d60d79596dc973be703872e
rainwolf <rainwolf@submanifold.be>
2023-12-02 18:24:23 +0800

    format

--- src/Pages/Table.js
+++ src/Pages/Table.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect, useRef } from 'react';
-import { connect } from 'react-redux';
+import React, {useState, useEffect, useRef} from 'react';
+import {connect} from 'react-redux';
 import PropTypes from 'prop-types';
 import Board from '../Board/Board';
 import Grid from '@material-ui/core/Grid';
@@ -8,82 +8,82 @@ import Snack from './Snack';
 import GameInfoPanel from "./GameInfoPanel";
 
 const mapStateToProps = state => {
-    return {
-        subscriber: state.subscriber
-        // subscriber: true
-    }
+   return {
+      subscriber: state.subscriber
+      // subscriber: true
+   }
 };
 
 const mapDispatchToProps = dispatch => {
-    return {
-        send_message: message => {
-            dispatch();
-        },
-    }
+   return {
+      send_message: message => {
+         dispatch();
+      },
+   }
 };
 
 
 const UnconnectedTable = (props) => {
 
-    const { table, subscriber } = props;
-    const [height, setHeight] = useState(0);
-    const [width, setWidth] = useState(0);
-    const ref = useRef(null);
+   const {table, subscriber} = props;
+   const [height, setHeight] = useState(0);
+   const [width, setWidth] = useState(0);
+   const ref = useRef(null);
 
-    // eslint-disable-next-line
-    useEffect(() => {
-        if (ref.current !== null) {
-            setHeight(ref.current.clientHeight - (!subscriber?90:0));
-            // console.log(ref.current.clientHeight, ref.current.clientWidth)
-            setWidth(ref.current.clientWidth);
-        } 
-    });
-    
+   // eslint-disable-next-line
+   useEffect(() => {
+      if (ref.current !== null) {
+         setHeight(ref.current.clientHeight - (!subscriber ? 90 : 0));
+         // console.log(ref.current.clientHeight, ref.current.clientWidth)
+         setWidth(ref.current.clientWidth);
+      }
+   });
 
-    return (
-        <div ref={ref} style={{height: '100%', width: '100%'}}>
 
-            <Grid container direction={'column'} alignItems={'center'} wrap={'nowrap'} style={{height: '100%', width: '100%'}}>
-                {!subscriber &&
-                <Grid item style={{ display:'inline-block',width:'970px',height:'90px' }}>
-                    <AdSense.Google
-                        client='ca-pub-3326997956703582'
-                        slot='3134525496'
-                        style={{ display:'inline-block',width:'970px',height:'90px' }}
-                        format=''
-                    />
-                </Grid>
-                }
-                <Grid item style={{flex: '1', minHeight: '0px'}}>
-        <div style={{height: '100%', width: Math.min(width, height + 600)}}>
-            <Grid container direction={'row'} alignItems={'stretch'} wrap={'nowrap'} style={{height: '100%'}}>
-                <Grid item style={{height:'100%'}}>
-                    <div style={{height: '100%', width: height}}>
-                        <Board />
-                    </div>
-                </Grid>
-                <Grid item style={{height:'100%', width: Math.min(600, width - height)}}>
-                    <Grid container direction={'column'} alignItems={'stretch'}  wrap={'nowrap'}
-                          style={{height:'100%', width: '100%'}}>
-                        <Grid item style={{height: '100%', borderWidth: '1px', borderStyle: 'solid'}}>
-                            <div style={{width: '100%', height: '100%', backgroundColor: '#fffff'}}>
-                                <GameInfoPanel/>
-                            </div>
+   return (
+      <div ref={ref} style={{height: '100%', width: '100%'}}>
+
+         <Grid container direction={'column'} alignItems={'center'} wrap={'nowrap'}
+               style={{height: '100%', width: '100%'}}>
+            {!subscriber &&
+               <Grid item style={{display: 'inline-block', width: '970px', height: '90px'}}>
+                  <AdSense.Google
+                     client='ca-pub-3326997956703582'
+                     slot='3134525496'
+                     style={{display: 'inline-block', width: '970px', height: '90px'}}
+                     format=''
+                  />
+               </Grid>
+            }
+            <Grid item style={{flex: '1', minHeight: '0px'}}>
+               <div style={{height: '100%', width: Math.min(width, height + 600)}}>
+                  <Grid container direction={'row'} alignItems={'stretch'} wrap={'nowrap'} style={{height: '100%'}}>
+                     <Grid item style={{height: '100%'}}>
+                        <div style={{height: '100%', width: height}}>
+                           <Board/>
+                        </div>
+                     </Grid>
+                     <Grid item style={{height: '100%', width: Math.min(600, width - height)}}>
+                        <Grid container direction={'column'} alignItems={'stretch'} wrap={'nowrap'}
+                              style={{height: '100%', width: '100%'}}>
+                           <Grid item style={{height: '100%', borderWidth: '1px', borderStyle: 'solid'}}>
+                              <div style={{width: '100%', height: '100%', backgroundColor: '#fffff'}}>
+                                 <GameInfoPanel/>
+                              </div>
+                           </Grid>
                         </Grid>
-                    </Grid>
-                </Grid>
-            </Grid>
+                     </Grid>
+                  </Grid>
 
-            <Snack/>
-        </div>
-                </Grid>
+                  <Snack/>
+               </div>
             </Grid>
-        </div>
-    );
+         </Grid>
+      </div>
+   );
 };
 
-UnconnectedTable.propTypes = {
-};
+UnconnectedTable.propTypes = {};
 
 
 const TableView = connect(mapStateToProps, mapDispatchToProps)(UnconnectedTable);
