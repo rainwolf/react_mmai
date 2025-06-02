f58d177f4ee383f10246589f60d13d678c2f848f
rainwolf <rainwolf@submanifold.be>
2025-06-02 20:49:16 +0700

    fix compile script

--- MMAIWASM/compile.sh
+++ MMAIWASM/compile.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-emcc Ai.cpp mmai.cpp -O3 -o ai.js -s WASM=1 -s "EXPORTED_RUNTIME_METHODS=['ccall']" \
+emcc Ai.cpp mmai.cpp -O3 -o ai.js -s WASM=1 -s "EXPORTED_RUNTIME_METHODS=['ccall', 'HEAPU8']" \
   -s TOTAL_STACK=32MB -s INITIAL_MEMORY=64MB \
   -s "EXPORTED_FUNCTIONS=['_malloc', '_free']" -std=c++11 --embed-file files/
